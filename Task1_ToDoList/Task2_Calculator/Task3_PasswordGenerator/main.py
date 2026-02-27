0789c0f784be5d965690b5d372b80e3989e0eab5
shalininehru-web <nshalininehru24@gmail.com>
2026-02-27 18:43:17 +0530

    Added Task 3 Password Generator

---
+++ Task1_ToDoList/Task2_Calculator/Task3_PasswordGenerator/main.py
@@ -0,0 +1,16 @@
+# CodSoft Internship Task 3
+# Password Generator
+
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
