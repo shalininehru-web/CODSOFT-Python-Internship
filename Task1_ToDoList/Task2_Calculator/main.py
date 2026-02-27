985bc78180d3fdc7aedf3468797e9e0410ba8774
shalininehru-web <nshalininehru24@gmail.com>
2026-02-27 18:42:36 +0530

    Added Task 2 Calculator

---
+++ Task1_ToDoList/Task2_Calculator/main.py
@@ -0,0 +1,41 @@
+# CodSoft Internship Task 2
+# Calculator
+
+def calculator():
+
+    while True:
+
+        print("\n===== CALCULATOR =====")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "5":
+            break
+
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == "1":
+            print("Result:", num1 + num2)
+
+        elif choice == "2":
+            print("Result:", num1 - num2)
+
+        elif choice == "3":
+            print("Result:", num1 * num2)
+
+        elif choice == "4":
+            if num2 == 0:
+                print("Cannot divide by zero")
+            else:
+                print("Result:", num1 / num2)
+
+        else:
+            print("Invalid choice")
+
+calculator()
